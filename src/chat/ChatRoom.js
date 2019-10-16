5ede359ea88308ce8dfa7da0bd54e48b6dadd963
Apirat Chiwakanit <apirat.c@kbtg.tech>
2019-10-16 14:41:44 +0700

    btn count

--- src/chat/ChatRoom.js
+++ src/chat/ChatRoom.js
@@ -3,14 +3,26 @@ import socketIOClient from 'socket.io-client'
 
 function ChatRoom() {
     const [socket, setSocket] = useState(null)
+    const [count, setCount] = useState(0)
 
     useEffect(() => {
         const socketIO = socketIOClient('localhost:8080')
+        socketIO.on('count', data => {
+            setCount(data.count)
+        })
         setSocket(socketIO)
-        socketIO.emit('emit', { data: 'tet' })
     }, [])
 
-    return <div>ChatRoom</div>
+    const handleClick = () => {
+        socket.emit('emit', null)
+    }
+
+    return (
+        <div>
+            <p>{count}</p>
+            <button onClick={handleClick}>Count</button>
+        </div>
+    )
 }
 
 export default ChatRoom
