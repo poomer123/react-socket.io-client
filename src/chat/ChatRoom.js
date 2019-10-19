3160192eba6920fe9488cd76995db8a649bf61d9
Apirat Chiwakanit <apirat.c@kbtg.tech>
2019-10-19 18:03:46 +0700

    create blank component

--- src/chat/ChatRoom.js
+++ src/chat/ChatRoom.js
@@ -1,29 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import socketIOClient from 'socket.io-client'
+import MessageList from './MessageList'
+import MessageForm from './MessageForm'
+import Message from './Message'
 
-function ChatRoom() {
-    const [socket, setSocket] = useState(null)
-    const [count, setCount] = useState(0)
-
-    useEffect(() => {
-        const socketIO = socketIOClient('localhost:8080')
-        socketIO.on('count', data => {
-            setCount(data.count)
-        })
-        setSocket(socketIO)
-    }, [])
-
-    const handleClick = () => {
-        socket.emit('emit', { count: count })
-        setCount(count + 1)
-    }
-
+export default function ChatRoom() {
     return (
         <div>
-            <p>{count}</p>
-            <button onClick={handleClick}>Count</button>
+            <MessageList />
+            <MessageForm />
+            <Message />
         </div>
     )
 }
-
-export default ChatRoom
